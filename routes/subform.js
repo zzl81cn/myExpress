f72d0aa9283d24dcb58888a64eb7de2ef473c061
zilong.zhou <zzl81cn@163.com>
2016-05-30 15:42:00 +0800

    add

--- routes/subform.js
+++ routes/subform.js
@@ -9,6 +9,31 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
+	res.render('subform', {
+		title: 'subform example'
+	});
+});
+
+router.post('/', function(req, res) {
+	// var userName = req.query.txtUserName,
+	// 		userPwd = req.query.txtUserPwd,
+	// 		userName2 = req.param('txtUserName'),
+	// 		userPwd2 = req.param('txtUserPwd');
+
+	// 		console.log('req.queryUserName:' + userName);
+	// 		console.log('req.queryUserPwd:' + userPwd);
+	// 		console.log('req.paramUserName:' + userName2);
+	// 		console.log('req.paramUserPwd:' + userPwd2);
+
+	var userName = req.body.txtUserName,
+			userPwd = req.body.txtUserPwd,
+			userName2 = req.param('txtUserName'),
+			userPwd2 = req.param('txtUserPwd');
+
+			console.log('req.bodyUserName:' + userName);
+			console.log('req.bodyUserPwd:' + userPwd);
+			console.log('req.paramUserName:' + userName2);
+			console.log('req.paramUserPwd:' + userPwd2);
 	res.render('subform', {title: 'Submit the form and sample receiving parameters.'});
 });
 
--- routes/usecrypto.js
+++ routes/usecrypto.js
@@ -7,9 +7,22 @@
 
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 
+// home page
 router.get('/', function(req, res) {
 	res.render('usecrypto', {title: 'An encrypted string'});
+});
+
+router.post('/', function(req, res) {
+	var userName = req.body.txtUserName,
+			userPwd = req.body.txtUserPwd;
+
+	var md5 = crypto.createHash('md5');
+	var en_upwd = md5.update(userPwd).digest('hex');
+
+	console.log('加密后的密码:' + en_upwd);
+	res.render('usecrypto', {title: '加密字符串序列'});
 });
 
 module.exports = router;
