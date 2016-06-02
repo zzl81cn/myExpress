e09c90ca9f83b9772b11938f39392c6fc4b90f91
zilong.zhou <zzl81cn@163.com>
2016-06-02 14:03:37 +0800

    modify mysql/escape

---
+++ mysql/escape.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-06-02 13:50:22
+ * @version $Id$
+ */
+
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: 'zzl81cn',
+	database: 'nodesample'
+});
+
+pool.getConnection(function(err, connection) {
+	connection.query('SELECT * FROM userinfo WHERE id = ' + '1 OR ID = 3', function(err, result) {
+		console.log(result);
+		connection.release();
+	});
+
+	connection.query('SELECT * FROM userinfo WHERE id =' + pool.escape('1 OR ID = 3'), function(err, result) {
+		console.log(result);
+		connection.release();
+	});
+});
