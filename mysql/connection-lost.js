36e70cdfa572f9adb0c4b40fe54c6e4254139f76
zilong.zhou <zzl81cn@163.com>
2016-06-02 12:03:58 +0800

    change connect-lost

--- mysql/connection-lost.js
+++ mysql/connection-lost.js
@@ -5,4 +5,32 @@
  * @version $Id$
  */
 
-var mysql = require('msyql');
+var mysql = require('mysql');
+var db_config = {
+	host: 'localhost',
+	user: 'root',
+	password: 'zzl81cn',
+	database: 'nodesample'
+};
+
+var connection;
+function handleDisconnect() {
+	connection = mysql.createConnection(db_config);
+	connection.connect(function(err) {
+		if(err) {
+			console.log('断线重连……' + new Date());
+			setTimeout(handleDisconnect, 2000);
+			return;
+		}
+		console.log('Connect success!');
+	});
+	connection.on('error', function(err) {
+		console.log('db error', err);
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleDisconnect();
+		} else {
+			throw err;
+		}
+	});
+}
+handleDisconnect();
