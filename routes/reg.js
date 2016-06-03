2a48e4130db879ea01983435c1c18be580342c03
zilong.zhou <zzl81cn@163.com>
2016-06-03 16:28:26 +0800

    update

--- routes/reg.js
+++ routes/reg.js
@@ -28,34 +28,34 @@ router.post('/', function(req, res) {
   //检查用户名是否已经存在
   User.getUserNumByName(newUser.username, function (err, results) {        
              
-      if (results != null && results[0]['num'] > 0) {
-          err = '用户名已存在';
-      }
+    if (results != null && results[0]['num'] > 0) {
+      err = '用户名已存在';
+    }
 
+    if (err) {
+      res.locals.error = err;
+      res.render('reg', { title: TITLE_REG });
+      return;
+    }
+
+    newUser.save(function (err,result) {
       if (err) {
-          res.locals.error = err;
-          res.render('reg', { title: TITLE_REG });
-          return;
+        res.locals.error = err;
+        res.render('reg', { title: TITLE_REG }); 
+        return;            
+      }        
+
+      if(result.insertId > 0)
+      {
+        res.locals.success = '注册成功,请点击   <a class="btn btn-link" href="/login" role="button"> 登录 </a>';
       }
-
-      newUser.save(function (err,result) {
-          if (err) {
-              res.locals.error = err;
-              res.render('reg', { title: TITLE_REG }); 
-              return;            
-          }        
-
-          if(result.insertId > 0)
-          {
-              res.locals.success = '注册成功,请点击   <a class="btn btn-link" href="/login" role="button"> 登录 </a>' ;
-          }
-          else
-          {
-              res.locals.error = err;
-          }
-         
-          res.render('reg', { title: TITLE_REG });
-          });    
+      else
+      {
+        res.locals.error = err;
+      }
+     
+      res.render('reg', { title: TITLE_REG });
+      });    
     });          
 });
 
