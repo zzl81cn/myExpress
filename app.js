d35d695deca2f1df4363e79e1aab136e809432a8
zilong.zhou <zzl81cn@163.com>
2016-06-06 15:32:25 +0800

    update

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var routes = require('./routes/index'),
-//var users = require('./routes/users');
     reg = require('./routes/reg'),
     login = require('./routes/login'),
     logout = require('./routes/logout');
