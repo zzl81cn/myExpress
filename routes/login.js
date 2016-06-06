6083ce26381b9565d26f7fe5bf744f1ff68d0736
zilong.zhou <zzl81cn@163.com>
2016-06-06 17:46:47 +0800

    1234

--- routes/login.js
+++ routes/login.js
@@ -25,24 +25,23 @@ router.post('/', function(req, res) {
 		return;
     }
 
-		userPwd = md5.update(userPwd).digest('hex');
-		if(results[0].UserName != userName || results[0].UserPass != userPwd){
-			res.locals.error = '用户名或密码有误';
-			res.render('login',{title:TITLE_LOGIN});
-			console.log(1);
-			return;
+	userPwd = md5.update(userPwd).digest('hex');
+	if(results[0].UserName != userName || results[0].UserPass != userPwd){
+		res.locals.error = '用户名或密码有误';
+		res.render('login',{title:TITLE_LOGIN});
+		console.log(1);
+		return;
+	} else {
+		if(isRem) {
+			res.cookie('islogin', userName, { maxAge: 60000 });
 		}
-		else {
-			if(isRem) {
-				res.cookie('islogin', userName, { maxAge: 60000 });
-			}
 
-			res.locals.username = userName;
-			req.session.username = res.locals.username;
-			console.log(req.session.username);
-			res.redirect('/');
-			return;
-		}
+		res.locals.username = userName;
+		req.session.username = res.locals.username;
+		console.log(req.session.username);
+		res.redirect('/');
+		return;
+	}
   });              
 });
 
