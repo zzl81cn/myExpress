167dd1c86d17ea753b94b2fda70f2f484cda6ea3
zilong.zhou <zzl81cn@163.com>
2016-06-08 17:02:05 +0800

    modify basic node.

--- routes/index.js
+++ routes/index.js
@@ -4,17 +4,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-// 	res.render('index', {
-// 		title: '<h1>Express</h1>',
-// 		users: [
-// 			{username: 'zzl81cn'}
-// 			,{username: 'zzl81cn01'}
-// 			,{username: 'zzl81cn001'}
-// 			,{username: 'zzl81cn0001'}
-// 		]
-// 	});
-// });
+/* router.get('/', function(req, res, next) {
+ 	res.render('index', {
+ 		title: '<h1>Express</h1>',
+ 		users: [
+ 			{username: 'zzl81cn'}
+ 			,{username: 'zzl81cn01'}
+ 			,{username: 'zzl81cn001'}
+ 			,{username: 'zzl81cn0001'}
+ 		]
+ 	});
+ });*/
 
 router.get('/', function(req, res, next) {
 	if (req.cookies.islogin) {
