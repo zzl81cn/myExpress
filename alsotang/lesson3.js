0386ce71cd033bf940eddc11d7772008683ac79a
zilong.zhou <zzl81cn@163.com>
2016-06-12 17:27:44 +0800

    update

--- alsotang/lesson3.js
+++ alsotang/lesson3.js
@@ -16,6 +16,9 @@ app.get('/', function(req, res, next) {
 			if(err) {
 				return next(err);
 			}
+//			sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
+//			就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
+//			剩下就都是 jquery 的内容了
 			var $ = cheerio.load(sres.text);
 			var items = [];
 			$('#topic_list .topic_title').each(function(idx, element) {
