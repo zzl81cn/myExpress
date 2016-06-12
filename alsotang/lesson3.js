945b0e81e10c8b3d990c1c47db3632e1da5e1b28
zilong.zhou <zzl81cn@163.com>
2016-06-12 12:04:03 +0800

    modify

--- alsotang/lesson3.js
+++ alsotang/lesson3.js
@@ -1,13 +1,18 @@
-// 使用 superagent 与 cheerio 完成简单爬虫
+/*使用 superagent 与 cheerio 完成简单爬虫
+学习使用 superagent 抓取网页
+学习使用 cheerio 分析网页*/
+//首先需要引入3个依赖，分别是：express,superagent,cheerio
 var express = require('express'),
-		superagent = require('superagent'),
-		cheerio = require('cheerio');
+	superagent = require('superagent'),
+	cheerio = require('cheerio');
 
 var app = express();
 
 app.get('/', function(req, res, next) {
+//	call superagent to capture the web.
 	superagent.get('https://cnodejs.org/')
 		.end(function(err, sres) {
+//			error process
 			if(err) {
 				return next(err);
 			}
