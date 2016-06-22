53896e202638bde92829ea3be48656cc63fa3f51
zilong.zhou <zzl81cn@163.com>
2016-06-22 16:10:50 +0800

    add lesson1.js(helloworld) in alsotang

---
+++ alsotang/lesson1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zhouzilong on 2016/6/22.
+ */
+
+const http = require('http');
+
+var server = http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type':'text/plain'});
+    response.end('HelloWorld!\n');
+}).listen(3000);
+
+console.log('Server is running at http://127.0.0.1:3000');
