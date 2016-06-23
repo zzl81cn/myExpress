dcf9e46fd9073d319e648da310a28ba21517f1cc
zilong.zhou <zzl81cn@163.com>
2016-06-23 17:10:35 +0800

    modify lesson3

--- alsotang/lesson3.js
+++ alsotang/lesson3.js
@@ -17,9 +17,7 @@ app.get('/', function(req, res, next) {
 			if(err) {
 				return next(err);
 			}
-//			sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
-//			就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-//			剩下就都是 jquery 的内容了
+			//sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`剩下就都是 jquery 的内容了
 
 			var $ch = cheerio.load(sres.text);
 			var items = [];
