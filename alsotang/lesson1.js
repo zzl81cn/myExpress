82d1ed0d7e6142f2faefb344c1d8cafdd1c15ce4
zilong.zhou <zzl81cn@163.com>
2016-06-23 16:06:46 +0800

    modify lesson1

--- alsotang/lesson1.js
+++ alsotang/lesson1.js
@@ -4,6 +4,7 @@
 
 const http = require('http');
 
+
 var server = http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type':'text/plain'});
     response.end('HelloWorld!\n');
