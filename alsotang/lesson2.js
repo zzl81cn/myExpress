71aea65f5675eec454feabb57da78d7deb27b54a
zilong.zhou <zzl81cn@163.com>
2016-06-23 15:26:15 +0800

    modify lesson2

--- alsotang/lesson2.js
+++ alsotang/lesson2.js
@@ -13,15 +13,9 @@ var utility = require('utility');
 */
 var app = express();
 
-/*app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，
- * 在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
- * 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request
- * 和 response。
- * req 中包含了浏览器传来的各种信息，比如 query、body、headers 啊之类的，
- * 都可以通过 req 对象访问到。
- * res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，
- * 比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send
- * 方法，向浏览器输出一个字符串。
+/*app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
+  req 中包含了浏览器传来的各种信息，比如 query、body、headers 啊之类的，都可以通过 req 对象访问到。
+  res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的send方法，向浏览器输出一个字符串。
 */
 app.get('/', function(req, res) {
 	var q = req.query.q;
