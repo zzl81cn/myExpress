a05fbbd6f2b871ca9f9640baa8c8341002f28763
zilong.zhou <zzl81cn@163.com>
2016-07-28 11:58:48 +0800

    1.Modify lesson1

--- alsotang/lesson1.js
+++ alsotang/lesson1.js
@@ -7,7 +7,7 @@ const http = require('http');
 
 var server = http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type':'text/plain'});
-    response.end('HelloWorld!\n');
+    response.end('HelloWorld!\nHello2');
 }).listen(3000);
 
 console.log('Server is running at http://127.0.0.1:3000');
