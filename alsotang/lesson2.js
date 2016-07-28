a5f9a86ef62c241cf273373b2ef3ab893a9a940e
zilong.zhou <zzl81cn@163.com>
2016-07-28 14:37:25 +0800

    1.Modify about express HelloWorld

--- alsotang/lesson2.js
+++ alsotang/lesson2.js
@@ -28,6 +28,7 @@ app.get('/', function(req, res) {
 /*定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，
  * 会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
 */
+//app.listen(3000);
 var server = app.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
