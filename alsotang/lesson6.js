5b9d243b0af6b09df6a66f6f7b3b11409e406381
zilong.zhou <zzl81cn@163.com>
2016-08-03 16:26:14 +0800

    1.Add leson6 & add should、mocha test framework.

--- alsotang/lesson6.js
+++ alsotang/lesson6.js
@@ -19,6 +19,15 @@
 //    console.log('fibonacci(' + n + ') is', fibonacci(n));
 //}
 var fibonacci = function(n){
+    if(typeof n !== 'number'){
+        throw new Error('n should be a Number');
+    }
+    if(n < 0){
+        throw new Error('n should >= 0');
+    }
+    if(n > 10){
+        throw new Error('n should <= 10');
+    }
     if(n === 0){
         return 0;
     } if(n === 1) {
--- test/fibonacci_test.js
+++ test/fibonacci_test.js
@@ -5,11 +5,21 @@
 var main = require('../alsotang/lesson6');
 var should = require('should');
 
-//describe('test/main.test.js', function () {
-//    it('should equal 55 when n === 10', function () {
-//        main.fibonacci(10).should.equal(55);
-//    });
-//});
+// Command
+
+//D:\Workspaces\javascript\Node\myExpress>mocha
+//
+//
+//test/fibonacci_test.js
+//√ should equal 0 when n === 0
+//√ should equal 1 when n === 1
+//√ should equal 55 when n === 10
+//√ should throw when n>10
+//√ should throw when n<0
+//√ should throw n isnt Number
+
+
+//6 passing (10ms)
 
 describe('test/fibonacci_test.js', function(){
     it('should equal 0 when n === 0', function(){
@@ -31,5 +41,9 @@ describe('test/fibonacci_test.js', function(){
             main.fibonacci(-1);
         }).should.throw('n should >= 0');
     });
-
+    it('should throw n isnt Number', function(){
+        (function(){
+            main.fibonacci("呵呵");
+        }).should.throw('n should be a Number');
+    });
 });
