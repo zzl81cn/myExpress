2102cc7f555376328708d4c863d153b93e18d92a
zzl81cn <zzl81cn@163.com>
2017-02-16 17:38:23 +0800

    利用“request"库来提交请求。

--- alsotang/lesson1b.js
+++ alsotang/lesson1b.js
@@ -5,16 +5,17 @@
  */
 var request = require('request');
 
-// 通过request请求服务器页面或数据接口
+// 通过request请求服务器页面或数据接口，http requrest(http://www.cnblogs.com/EricaMIN1987_IT/p/3654233.html)
 var url = [
 	'http://www.galaxybigdata.com/warehouse/publish/list?fromSystem=1&fromType=&name=&orderBy=1&pageNum=1&pageSize=20&publishToShop=1&status=',
 	'http://rap.taobao.org/mockjsdata/11697/categorylist'];
 
-/*request(url[0], function (error, response, body) {
+request(url[1], function (error, response, body) {
 	if (!error && response.statusCode == 200) {
 		console.log(body) // Show the HTML for the Google homepage.
 	}
-});*/
+});
+
 
 /*var http = require('http'),
 	opts = {
@@ -25,6 +26,7 @@ http.request(opts, function (res) {
 	// https://www.zhihu.com/question/26843761
 	console.log(res)
 })*/
+/*
 var http = require('http');
 var url = require('url');
 var util = require('util');
@@ -54,4 +56,4 @@ var req = http.request(options, function(res){
 req.on('error', function(err){
 	util.log('REQUEST ERROR: ' + err);
 });
-req.end();
+req.end();*/
