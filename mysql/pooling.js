f9c066ce4a5776a712eb29fd7371a4030fcea33b
zzl81cn <zzl81cn@163.com>
2017-05-02 15:43:14 +0800

    Node mysql(connectionPool).

--- mysql/pooling.js
+++ mysql/pooling.js
@@ -13,30 +13,36 @@ var pool = mysql.createPool({
 	database: 'nodesample'
 });
 
-// pool.on('connection', function(connection) {
-// 	connection.query('SET SESSION auto_increment_increment=1');
-// });
+/*pool.on('connection', function(connection) {
+	connection.query('SET SESSION auto_increment_increment=1');
+});*/
 
 //直接使用
-// pool.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-//   if (err) throw err;
+pool.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
 
-//   console.log('The solution is: ', rows[0].solution);
-// });
+  console.log('The solution is: ', rows[0].solution);
+});
 
 //共享
-pool.getConnection(function(err, connection) {
-  // connected! (unless `err` is set)
-  connection.query('select * from userinfo', function(err, result) {
-  	console.log(result);
-  	// connection.release();
-  });
+pool.getConnection(function (err, connection) {
+	// connected! (unless `err` is set)
+	connection.query('select * from userinfo', function (err, result) {
+		console.log(result);
+		connection.release();
+	});
 
-  connection.query('SELECT * FROM userinfo', function(err, result) {
-  	console.log(result);
-  	connection.release();
-  });
+	/*connection.query('SELECT * FROM userinfo', function (err, result) {
+		console.log(result);
+		connection.release();
+	});*/
 });
+
+/*pool.end(function (err) {
+	if(err) {
+		console.log('End field!');
+	} console.log('ok!')
+});*/
 
 // waitForConnections
 // 当连接池没有连接或超出最大限制时，设置为true且会把连接放入队列，设置为false会返回error
