26127c89aae2d98d78221d8c14865ab974a13d84
zzl81cn <zzl81cn@163.com>
2017-05-27 09:57:42 +0800

    Modify node helloworld.

--- alsotang/lesson1.js
+++ alsotang/lesson1.js
@@ -2,13 +2,10 @@
  * Created by zhouzilong on 2016/6/22.
  */
 
-
-// 20170424
 const http = require('http');
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Cotent-Type': 'text/html;charset=utf-8'});
-	res.end('HelloWorld!\nWelcome join to NodeJS world!');
+	res.end('Hello!\nWelcome join to nodejs world!');
 }).listen(3000);
-
-console.log('Server is running at http://localhost:3001/ or http://127.0.0.1:3000/');
+console.log('Server is running at http://localhost:3000/ or http://127.0.0.1:3000/');
