04270b8083684959b906fb40d9358dc3c031611e
zzl81cn <zzl81cn@163.com>
2018-08-29 16:55:27 +0800

    增加Request请求示例

--- my-express4/routes/login.js
+++ my-express4/routes/login.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+
+const reqURL ={
+  rap: 'http://rapapi.org/mockjsdata/13161/multiselect_h',
+  bdg: 'http://rapapi.org/mockjsdata/11697/categorylist',
+  easyDynamic: 'https://easy-mock.com/mock/5a0d2eb685e6ba3feeead78c/example/mock'
+};
 
 router.get('/', function(req, res) {
-  res.render('login', {title: 'login page'});
+  // request('http://www.baidu.com', function(error, response, body){
+  request(reqURL.easyDynamic, function(error, response, body){
+    if(!error && response.statusCode == 200) {
+      console.log(body); // Show the HTML for the baidu homepage.
+    }
+  })
+  res.render('login', {title: 'login page2'});
 });
 
 module.exports = router;
