9a2d621f3938d082e8caaed9925339cb344151b9
zzl81cn <zzl81cn@163.com>
2018-09-04 16:54:07 +0800

    增加http请求。

---
+++ dom-full-stack/README.md
@@ -0,0 +1,2 @@
+[多页架构](https://segmentfault.com/a/1190000008644787)
+
--- dom-full-stack/routes/index.js
+++ dom-full-stack/routes/index.js
@@ -1,12 +1,64 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+function getPromise(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, responese, body) => {
+      if(!error && responese.statusCode == 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    })
+  })
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    pagename: 'awesome people',
-    authors: ['Paul', 'Jim', 'Jane']
-  });
+  let url = 'http://rap2api.taobao.org/app/mock/data/382300';
+  getPromise(url).then((data) => {
+    const result = JSON.parse(data);
+    console.log('HTTP method responese data is: ', JSON.stringify(result));
+    res.render('index', {
+      pagename: 'awesome people',
+      authors: ['Paul', 'Jim', 'Jane'],
+      data: result
+    });
+
+  })
 });
+
+/* 
+  example
+  {
+    "string": "★★★★★★★★",
+    "number": 65,
+    "boolean": true,
+    "regexp": "jU7",
+    "function": 0.021521137085384945,
+    "array": [
+      {
+        "foo": 1,
+        "bar": "★★★★"
+      },
+      {
+        "foo": 2,
+        "bar": "★★★★★★★★★★"
+      }
+    ],
+    "items": [
+      1,
+      true,
+      "hello",
+      "zPbvcr26tx"
+    ],
+    "object": {
+      "foo": 1,
+      "bar": "★★★"
+    },
+    "placeholder": "Imeafjs Tmb Njfrjuz Vvelnrd Wxn"
+  }
+*/
 
 module.exports = router;
