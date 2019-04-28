410e9dc10a3e33eeefb8eafd836a96302f95ff9c
zzl81cn <zzl81cn@163.com>
2019-04-28 23:24:20 +0800

    update prod at uglifyjs-webpack-p,optimize-css-ass

--- express-multiple-entry/build/webpack.prod.js
+++ express-multiple-entry/build/webpack.prod.js
@@ -1,4 +1,30 @@
 const merge = require('webpack-merge');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const webpackBaseConfig = require('./webpack.base');
 
-module.exports = merge(webpackBaseConfig, {});
+module.exports = merge(webpackBaseConfig, {
+    mode: 'production',
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    compress: {
+                        warnings: false,
+                        drop_debugger: true,
+                        drop_console: false
+                    }
+                }
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css'
+        })
+    ]
+});
